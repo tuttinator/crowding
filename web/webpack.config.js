b49f9b1a9c1ecfb73a7ca352aa08bfbf6fa3e41d
Caleb <caleb.tutty@nzherald.co.nz>
2016-07-30 16:43:22 +1200

    Mapbox gl deps and webpack config

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,4 +1,5 @@
 var webpack = require('webpack');
+var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var PRODUCTION = (process.env.NODE_ENV === 'production')
 
@@ -9,6 +10,13 @@ module.exports = {
     path: './dist',
     filename: PRODUCTION ? 'bundle.min.js' : 'bundle.js',
     publicPath: '/'
+  },
+  resolve: {
+    extensions: ['', '.js'],
+    alias: {
+      webworkify: 'webworkify-webpack',
+      'mapbox-gl': path.resolve('./node_modules/mapbox-gl/dist/mapbox-gl.js')
+    }
   },
   devServer: {
     inline: true,
@@ -23,6 +31,14 @@ module.exports = {
         query: {
           presets: ['es2015', 'react']
         }
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      {
+        test: /mapbox-gl.+\.js$/,
+        loader: 'transform/cacheable?brfs'
       },
       {
         test: /\.css/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
